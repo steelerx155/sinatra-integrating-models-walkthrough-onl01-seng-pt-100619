6498df7d2c4244138e07e9a42afc28e1f2b83bfb
Randy Steele <steelerx155@gmail.com>
2020-01-06 19:54:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,15 +1,14 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
-
 class App < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+
   post '/' do
-   @analyzed_text = TextAnalyzer.new(params[:user_text])
- 
-   erb :results
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+
+    erb :results
   end
 end
