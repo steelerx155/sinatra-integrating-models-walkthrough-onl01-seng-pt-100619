7dcaba8d60a2f2157e11a2336d947ba163836531
Randy Steele <steelerx155@gmail.com>
2020-01-06 19:47:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,12 +6,10 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
   post '/' do
-  text_from_user = params[:user_text]
+   @analyzed_text = TextAnalyzer.new(params[:user_text])
  
-  @analyzed_text = TextAnalyzer.new(text_from_user)
- 
-  erb :results
+   erb :results
   end
 end
